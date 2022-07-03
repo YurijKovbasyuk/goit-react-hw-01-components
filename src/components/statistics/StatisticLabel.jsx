daf72ff61fd2319dd5d2fdf0fd3785b8be6a07ee
YurijKovbasyuk <93015127+YurijKovbasyuk@users.noreply.github.com>
2022-07-03 10:34:13 +0300

    q

--- src/components/statistics/StatisticLabel.jsx
+++ src/components/statistics/StatisticLabel.jsx
@@ -2,12 +2,23 @@ import css from 'components/statistics/statistics.module.css';
 
 const StatisticLabel = ({ stats }) => {
   // console.log('1', stats[0]);
+
   return stats.map(stat => (
-    <li className={css.item} key={stat.id}>
+    <li
+      className={css.item}
+      key={stat.id}
+      style={{ backgroundColor: getRandomHexColor() }}
+    >
       <span className={css.label}>{stat.label}</span>
       <span className={css.percentage}>{stat.percentage}</span>
     </li>
   ));
 };
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
 export default StatisticLabel;
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -5,7 +5,7 @@ import StatisticLabel from 'components/statistics/StatisticLabel';
 const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title !== undefined && <h2 className={css.title}>{title}</h2>}
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.statList}>
         <StatisticLabel key={stats.id} stats={stats} />
       </ul>
